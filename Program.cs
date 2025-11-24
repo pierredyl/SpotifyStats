91bbbacc95495c769915cf6fcec5d58637c180e0
Dylan Pierre <dylanpierre004@gmail.com>
2025-11-23 21:43:22 -0800

    Updated frontendUrl variable to reflect accurately

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddScoped<ArtistService>();
 builder.Services.Configure<SpotifySettings>(builder.Configuration.GetSection("Spotify"));
 
 //CORS
-var frontendUrl = builder.Configuration["FrontendUrl"] ?? "http://localhost:5173";
+var frontendUrl = builder.Configuration["frontendUrl"] ?? "http://localhost:5173";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
